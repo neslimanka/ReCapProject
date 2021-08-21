13bed7b3a3738c046365c87d0aaaafa380586a7b
neslimanka <43746150+neslimanka@users.noreply.github.com>
2021-08-21 23:41:49 +0300

    Autofact desteği eklendi

--- ReCapProject/WebAPI/Startup.cs
+++ ReCapProject/WebAPI/Startup.cs
@@ -30,25 +30,25 @@ namespace WebAPI
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
-            services.AddSingleton<ICarService, CarManager>();//Bana arka planda bir servis olustur.
-            // Ben diyorum ki IoC ye , birisi senden IProductServis isterse ona arka planda bir tane ProductManager oluştur 
-            //onu ver 
-            services.AddSingleton<ICarDal, EfCarDal>();
-            //----------------------------------------------
-            services.AddSingleton<IBrandService, BrandManager>();
-            services.AddSingleton<IBrandDal, EfBrandDal>();
-            //----------------------------------------------
-            services.AddSingleton<IColorService, ColorManager>();
-            services.AddSingleton<IColorDal, EfColorDal>();
-            //----------------------------------------------
-            services.AddSingleton<ICustomerService, CustomerManager>();
-            services.AddSingleton<ICustomerDal, EfCustomerDal>();
-            //----------------------------------------------
-            services.AddSingleton<IRentalService, RentalManager>();
-            services.AddSingleton<IRentalDal, EfRentalDal>();
-            //----------------------------------------------
-            services.AddSingleton<IUserService, UserManager>();
-            services.AddSingleton<IUserDal, EfUserDal>();
+            //services.AddSingleton<ICarService, CarManager>();//Bana arka planda bir servis olustur.
+            //// Ben diyorum ki IoC ye , birisi senden IProductServis isterse ona arka planda bir tane ProductManager oluştur 
+            ////onu ver 
+            //services.AddSingleton<ICarDal, EfCarDal>();
+            ////----------------------------------------------
+            //services.AddSingleton<IBrandService, BrandManager>();
+            //services.AddSingleton<IBrandDal, EfBrandDal>();
+            ////----------------------------------------------
+            //services.AddSingleton<IColorService, ColorManager>();
+            //services.AddSingleton<IColorDal, EfColorDal>();
+            ////----------------------------------------------
+            //services.AddSingleton<ICustomerService, CustomerManager>();
+            //services.AddSingleton<ICustomerDal, EfCustomerDal>();
+            ////----------------------------------------------
+            //services.AddSingleton<IRentalService, RentalManager>();
+            //services.AddSingleton<IRentalDal, EfRentalDal>();
+            ////----------------------------------------------
+            //services.AddSingleton<IUserService, UserManager>();
+            //services.AddSingleton<IUserDal, EfUserDal>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
