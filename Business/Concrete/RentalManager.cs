466425c8783d7b3c6cc74e8624a7a268ca802e00
neslimanka <43746150+neslimanka@users.noreply.github.com>
2021-08-31 21:19:49 +0300

    Changes

---
+++ Business/Concrete/RentalManager.cs
@@ -0,0 +1,45 @@
+﻿using Business.Abstract;
+using Business.Constant;
+using Core.Utilities.Results;
+using DataAccess.Abstract;
+using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Business.Concrete
+{
+    public class RentalManager : IRentalService
+    {
+        IRentalDal _rentalDal;
+        public RentalManager(IRentalDal rentaldal)
+        {
+            _rentalDal = rentaldal;
+        }
+
+        public IResult Add(Rental rental)
+        {
+            return new SuccessResult(Messages.Addeded);
+        }
+
+        public IResult Delete(Rental rental)
+        {
+            return new SuccessResult(Messages.Deleted);
+        }
+
+        public IDataResult<List<Rental>> GetAll()
+        {
+            return new SuccessDataResult<List<Rental>>(_rentalDal.GetAll(), Messages.Listed);
+        }
+
+        public IDataResult<Rental> GetById(int id)
+        {
+            return new SuccessDataResult<Rental>(_rentalDal.Get(re => re.Id == id), Messages.Listed);
+        }
+
+        public IResult Update(Rental rental)
+        {
+            return new SuccessResult(Messages.Updated);
+        }
+    }
+}
---
+++ WebAPI/Controllers/CustomersController.cs
@@ -0,0 +1,64 @@
+﻿using Business.Abstract;
+using Entities.Concrete;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CustomersController : ControllerBase
+    {
+
+        ICustomerService _customerservice;
+        public CustomersController(ICustomerService customerService)
+        {
+            _customerservice = customerService;
+        }
+        [HttpPost("Add")]
+        public IActionResult Add(Customer customer)
+        {
+            var result = _customerservice.Add(customer);
+            if (result.Success)
+            {
+                return Ok(result);
+            }
+            return BadRequest(result);
+        }
+        [HttpPost("Delete")]
+        public IActionResult Delete(Customer customer)
+        {
+            var result = _customerservice.Delete(customer);
+            if (result.Success)
+            {
+                return Ok(result);
+            }
+            return BadRequest(result);
+        }
+        [HttpPost("Update")]
+        public IActionResult Update(Customer customer)
+        {
+            var result = _customerservice.Update(customer);
+            if (result.Success)
+            {
+                return Ok(result);
+            }
+            return BadRequest(result);
+        }
+
+        [HttpGet("GetAll")]
+        public IActionResult GetAll()
+        {
+            var result = _customerservice.GetAll();
+            if (result.Success)
+            {
+                return Ok(result.Data);
+            }
+            return BadRequest(result.Message);
+        }
+    }
+}
