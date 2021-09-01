5b2a1afc75437dafacc43d127c6c47c1c3c85d77
neslimanka <43746150+neslimanka@users.noreply.github.com>
2021-09-01 16:54:17 +0300

    FluentValidation desteği eklendi.

---
+++ Core/CrossCuttingConcerns/Validation/ValidationTool.cs
@@ -0,0 +1,22 @@
+﻿using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Core.CrossCuttingConcerns.Validation
+{
+    public static class ValidationTool
+    {
+        public static void Validate(IValidator validator, object entity)
+        {
+            var context = new ValidationContext<object>(entity);//product türü için bir doğrulama
+
+            var result = validator.Validate(context);
+            if (!result.IsValid)
+            {
+                throw new ValidationException(result.Errors);
+
+            }
+        }
+    }
+}
